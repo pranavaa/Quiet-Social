a3cc2bc6e52f3029218ab2ea4164be215f242a2f
Arpith Siromoney <arpith@sl22.cmi.ac.in>
2010-03-28 19:27:50 +0530

    hmac-sha signature

--- simpledb.py
+++ simpledb.py
@@ -3,9 +3,22 @@ import hmac
 import hashlib
 import datetime
 
-id = ""
-action = ""
-timestamp = datetime.datetime.now()
-maker = hmac.new(key, msg, hashlib.sha1)
-digest = maker.digest()
-print base64.encodestring(digest)
+def sign(request) {
+    maker = hmac.new(key, request, hashlib.sha1)
+    digest = maker.digest()
+    signature = base64.encodestring(digest)
+    return signature
+    }
+
+def request(action,params) {
+    url = "https://sdb.amazonaws.com"
+    AWSAccessKeyId = "AKIAIVZPLNT4II4LVD4Q"
+    version = "2009-04-15"
+    signatureVersion = "2"
+    signatureMethod = "HmacSHA256"
+    request = url + '?Action=' + action
+    for p, v in params.iteritems() {
+        request += '&' + p + '=' + v
+    signature = sign(request)
+    timestamp = datetime.datetime.now()
+    }
